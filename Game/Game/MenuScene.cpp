326d1f5f82ccf66eb0e81565c9adfb2565c0b67f
SGAoo7 <j.petter111@gmail.com>
2018-01-18 10:49:51 +0100

    63. 18-01-2018
    Things done

--- Game/Game/MenuScene.cpp
+++ Game/Game/MenuScene.cpp
@@ -22,7 +22,10 @@ void MenuScene::Setup() {
 	GOUILogoHolder.SetDrawIndex(0);
 	GOUIAudioHolder.SetDrawIndex(0);
 	GOUIButtonHolder.SetDrawIndex(0);
-	GOUIRefHolder.SetDrawIndex(0);
+	GOBackgroundHolder.SetDrawIndex(1);
+
+	//Adding components to Background holder GameObject
+	GOBackgroundHolder.AddComponent(&backgroundSprite);
 
 	//Adding components to UI AudioManager GameObject
 	GOUIAudioHolder.AddComponent(&audioSwitchSprite);
@@ -30,32 +33,23 @@ void MenuScene::Setup() {
 
 	//Adding components to UILogo holder GameObject
 	GOUILogoHolder.AddComponent(&logoPrism);
-	GOUILogoHolder.AddComponent(&logoPrismText);
 
 	//Adding components to UIButton holder GameObject
 	GOUIButtonHolder.AddComponent(&buttonStart);
-	GOUIButtonHolder.AddComponent(&buttonShop);
-	GOUIButtonHolder.AddComponent(&buttonCredits);
-	GOUIButtonHolder.AddComponent(&buttonQuit);
-
-	//Adding components to ref holder GameObject
-	GOUIRefHolder.AddComponent(&refSprite);
+	GOUIButtonHolder.AddComponent(&buttonShopt);
+	GOUIButtonHolder.AddComponent(&buttonCreditss);
+	GOUIButtonHolder.AddComponent(&buttonQuits);
 
 	//Setting standard values for logoPrism
 	logoPrism.setScale(0.4f, 0.4f);
 	logoPrism.setOrigin(logoPrism.getTexture()->getSize().x / 2, logoPrism.getTexture()->getSize().y / 2);
 	logoPrism.setPosition(Window::GetWindowSize().x / 2, 220);
 
-	//Setting standard values for logoPrismText
-	logoPrismText.setScale(0.4f, 0.4f);
-	logoPrismText.setOrigin(logoPrismText.getTexture()->getSize().x / 2, logoPrismText.getTexture()->getSize().y / 2);
-	logoPrismText.setPosition(Window::GetWindowSize().x / 2, logoPrism.getPosition().y + (logoPrismText.getTexture()->getSize().y));
-
 	//Push back all the menu buttons
 	menuButton.push_back(&buttonStart);
-	menuButton.push_back(&buttonShop);
-	menuButton.push_back(&buttonCredits);
-	menuButton.push_back(&buttonQuit);
+	menuButton.push_back(&buttonShopt);
+	menuButton.push_back(&buttonCreditss);
+	menuButton.push_back(&buttonQuits);
 
 	//Set scale and position for every menu button
 	for (auto it = menuButton.begin(); it != menuButton.end(); it++) {
@@ -64,24 +58,19 @@ void MenuScene::Setup() {
 	}
 
 	//Set position of buttons
-	buttonShop.GetCurrentButtonSprite().setPosition(buttonShop.GetCurrentButtonSprite().getPosition().x + buttonShop.GetCurrentButtonSprite().getTexture()->getSize().x / 2, buttonShop.GetCurrentButtonSprite().getPosition().y);
-	buttonCredits.GetCurrentButtonSprite().setPosition(buttonCredits.GetCurrentButtonSprite().getPosition().x, buttonCredits.GetCurrentButtonSprite().getPosition().y + 60);
-	buttonQuit.GetCurrentButtonSprite().setPosition(buttonQuit.GetCurrentButtonSprite().getPosition().x, buttonQuit.GetCurrentButtonSprite().getPosition().y + 60);
-	buttonQuit.GetCurrentButtonSprite().setPosition(buttonQuit.GetCurrentButtonSprite().getPosition().x + buttonQuit.GetCurrentButtonSprite().getTexture()->getSize().x / 2, buttonQuit.GetCurrentButtonSprite().getPosition().y);
-
-	//Setting standard values of refSprite
-	refSprite.setScale(0.6f, 0.6f);
-	refSprite.setOrigin(refSprite.getTexture()->getSize().x / 2, refSprite.getTexture()->getSize().y / 2);
-	refSprite.setPosition(Window::GetWindowSize().x / 2, Window::GetWindowSize().y - 30);
+	buttonShopt.GetCurrentButtonSprite().setPosition(buttonShopt.GetCurrentButtonSprite().getPosition().x + buttonShopt.GetCurrentButtonSprite().getTexture()->getSize().x / 2, buttonShopt.GetCurrentButtonSprite().getPosition().y);
+	buttonCreditss.GetCurrentButtonSprite().setPosition(buttonCreditss.GetCurrentButtonSprite().getPosition().x, buttonCreditss.GetCurrentButtonSprite().getPosition().y + 60);
+	buttonQuits.GetCurrentButtonSprite().setPosition(buttonQuits.GetCurrentButtonSprite().getPosition().x, buttonQuits.GetCurrentButtonSprite().getPosition().y + 60);
+	buttonQuits.GetCurrentButtonSprite().setPosition(buttonQuits.GetCurrentButtonSprite().getPosition().x + buttonQuits.GetCurrentButtonSprite().getTexture()->getSize().x / 2, buttonQuits.GetCurrentButtonSprite().getPosition().y);
 
 	//Load audio textures
-	audioTextureNotMuted.loadFromFile("../Assets/Art/Images/SoundNotMutedImage.png");
-	audioTextureMuted.loadFromFile("../Assets/Art/Images/SoundMutedImage.png");
+	audioTextureNotMuted.loadFromFile("../Assets/Art/Menu/SoundNotMutedImage.png");
+	audioTextureMuted.loadFromFile("../Assets/Art/Menu/SoundMutedImage.png");
 
 	//Setting standard values of audioSwitchSprite
 	audioSwitchSprite.setTexture(audioTextureNotMuted);
 	audioSwitchSprite.setScale(0.6f, 0.6f);
-	audioSwitchSprite.setPosition(5, 5);
+	audioSwitchSprite.setPosition(15, 730);
 }
 
 //Override Start method from base Scene class
@@ -120,21 +109,21 @@ void MenuScene::Update(float deltaTime) {
 		AudioManager::GetSound("ButtonClickSound")->play();
 	}
 	//Load scene if button is pressed
-	if (buttonShop.IsClicked()) {
+	if (buttonShopt.IsClicked()) {
 		//Load scene
 		SceneManager::LoadScene(4);
 		//Play click sound
 		AudioManager::GetSound("ButtonClickSound")->play();
 	}
 	//Load scene if button is pressed
-	if (buttonCredits.IsClicked()) {
+	if (buttonCreditss.IsClicked()) {
 		//Load scene
 		SceneManager::LoadScene(3);
 		//Play click sound
 		AudioManager::GetSound("ButtonClickSound")->play();
 	}
 	//Quit game if quit button is pressed
-	if (buttonQuit.IsClicked()) {
+	if (buttonQuits.IsClicked()) {
 		//Set closed boolean
 		Window::IsClosed = true;
 		//Play click sound
--- Game/Game/MenuScene.h
+++ Game/Game/MenuScene.h
@@ -28,12 +28,16 @@ public:
 	void Update(float deltaTime);
 
 private:
+	//Background Holder
+	GameObject GOBackgroundHolder = GameObject("BackgroundHolder", *this);
+	//Background sprite
+	BC::Sprite backgroundSprite = BC::Sprite("../Assets/Art/Menu/Background.png");
+
+
 	//UI Logo Holder
 	GameObject GOUILogoHolder = GameObject("UILogoHolder", *this);
 	//Logo prism sprite
-	BC::Sprite logoPrism = BC::Sprite("../Assets/Prism.png");
-	//Logo prism text sprite
-	BC::Sprite logoPrismText = BC::Sprite("../Assets/Menu/PrismText.png");
+	BC::Sprite logoPrism = BC::Sprite("../Assets/Art/Menu/Logo.png");
 
 	///Audio components
 	BC::Music* backgroundMusic = new BC::Music("../Assets/Audio/BackgroundMusic.wav");
@@ -49,18 +53,13 @@ private:
 	//UI Button Holder
 	GameObject GOUIButtonHolder = GameObject("UIButtonHolder", *this);
 	//Start button
-	BC::Button buttonStart = BC::Button("../Assets/Menu/Buttons/Start/ButtonStartIdle.png", "../Assets/Menu/Buttons/Start/ButtonStartHovered.png", "../Assets/Menu/Buttons/Start/ButtonStartPressed.png");
+	BC::Button buttonStart = BC::Button("../Assets/Art/Menu/Buttons/Start/ButtonStartIdle.png", "../Assets/Art/Menu/Buttons/Start/ButtonStartHovered.png", "../Assets/Art/Menu/Buttons/Start/ButtonStartPressed.png");
 	//Shop button
-	BC::Button buttonShop = BC::Button("../Assets/Menu/Buttons/Shop/ButtonShopIdle.png", "../Assets/Menu/Buttons/Shop/ButtonShopHovered.png", "../Assets/Menu/Buttons/Shop/ButtonShopPressed.png");
+	BC::Button buttonShopt = BC::Button("../Assets/Art/Menu/Buttons/Shop/ButtonShopIdle.png", "../Assets/Art/Menu/Buttons/Shop/ButtonShopHovered.png", "../Assets/Art/Menu/Buttons/Shop/ButtonShopPressed.png");
 	//Credits button
-	BC::Button buttonCredits = BC::Button("../Assets/Menu/Buttons/Credits/ButtonCreditsIdle.png", "../Assets/Menu/Buttons/Credits/ButtonCreditsHovered.png", "../Assets/Menu/Buttons/Credits/ButtonCreditsPressed.png");
+	BC::Button buttonCreditss = BC::Button("../Assets/Art/Menu/Buttons/Credits/ButtonCreditsIdle.png", "../Assets/Art/Menu/Buttons/Credits/ButtonCreditsHovered.png", "../Assets/Art/Menu/Buttons/Credits/ButtonCreditsPressed.png");
 	//Quit button
-	BC::Button buttonQuit = BC::Button("../Assets/Menu/Buttons/Quit/ButtonQuitIdle.png", "../Assets/Menu/Buttons/Quit/ButtonQuitHovered.png", "../Assets/Menu/Buttons/Quit/ButtonQuitPressed.png");
+	BC::Button buttonQuits = BC::Button("../Assets/Art/Menu/Buttons/Quit/ButtonQuitIdle.png", "../Assets/Art/Menu/Buttons/Quit/ButtonQuitHovered.png", "../Assets/Art/Menu/Buttons/Quit/ButtonQuitPressed.png");
 	//List of menu buttons
 	std::list<BC::Button*> menuButton;
-
-	//UI Reference holder GameObject
-	GameObject GOUIRefHolder = GameObject("RefHolder", *this);
-	//Reference sprite
-	BC::Sprite refSprite = BC::Sprite("../Assets/Menu/JP2017.png");
 };
